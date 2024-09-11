5867140341368c15d6d9983e5e09ce68155299c0
VG_VITIN <cleitinhadaquebrada05@gmail.com>
2024-09-11 20:09:39 -0300

    aula 10-09

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,3 @@
 $(document).ready(function() {
-    alert();
-var nr_c  = " ";
-
-    $('.bt_calc').click(function() {
-        var nr = $(this).text();
-
-        nr_c = nr_c.concat(nr);
-
-        $('#txt_display').text(nr_c)
-         });
-    });
+    
+});
